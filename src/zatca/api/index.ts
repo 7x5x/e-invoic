9047b751501d5837d35463fa9fba7d90905ffa10
Ali <alialjabal4@gmail.com>
2024-10-31 15:33:25 +0300

    a

--- src/zatca/api/index.ts
+++ src/zatca/api/index.ts
@@ -5,8 +5,8 @@ import { log } from "../../logger/index.js";
 import { handleInvoiceError } from "./handelError.js";
 const settings = {
   API_VERSION: "V2",
-  // SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/simulation",
-  SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/core",
+  SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/simulation",
+  // SANDBOX_BASEURL: "https://gw-fatoora.zatca.gov.sa/e-invoicing/core",
   PRODUCTION_BASEURL: "TODO",
 };
 
