25ab354afc92180fd362c5182243c34031b76f0f
Ali <alialjabal4@gmail.com>
2025-03-26 10:38:19 +0300

    refactor invoice response to include detailed validation results and clearance status

--- controller/invoice.ts
+++ controller/invoice.ts
@@ -42,7 +42,14 @@ export const invoiceRouter = async (req: Request, res: Response) => {
 
     const result = await main(value);
     saveInvoice(filename, result.clearedInvoice);
-    return res.status(200).json("result");
+    // return res.status(200).json(result);
+    return res.status(200).json({
+      validationResults: result.validationResults,
+      clearanceStatus: result.clearanceStatus,
+      hash: result.hash,
+      qr: result.qr,
+      invoiceID: value.props.invoice_serial_number
+    });
   } catch (error) {
     error.Statcode === 202 && saveInvoice(filename, error.clearedInvoice);
 
